c8dd34e9bc50ebe7134ba49712f8ba860b3f7cd6
Leander Paul F <leanderpaul97@outlook.com>
2021-01-19 16:22:04 +0000

    [Hackerrank][Array & String] Monk and Rotation

--- hackerearth/array-and-string/monk-and-rotation/Solution.js
+++ hackerearth/array-and-string/monk-and-rotation/Solution.js
@@ -29,7 +29,11 @@ function main(input) {
   const numberOfTestCases = inputArr.shift();
   for (let index = 0; index < numberOfTestCases; index++) {
     const arrLen = parseInt(inputArr.shift());
-    const steps = parseInt(inputArr.shift());
-    const arr = input.slice();
+    const steps = parseInt(inputArr.shift()) % arrLen;
+    const rotation = arrLen - steps;
+    const arr = inputArr.slice(0, arrLen);
+    inputArr = inputArr.slice(arrLen);
+    const rotatedArr = [...arr.slice(rotation), ...arr.slice(0, rotation)];
+    console.log(rotatedArr.join(' '));
   }
 }
